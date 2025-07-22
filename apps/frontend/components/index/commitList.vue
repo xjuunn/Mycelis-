389963556f86ee73149768be84ec09f4039ad0e5
Junhsiun <xjuunn@gmail.com>
2025-07-22 15:49:44 +0800

    feat: 添加提交记录列表组件 支持分页和更新详情显示 微调提交记录列表样式

---
+++ apps/frontend/components/index/commitList.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="p-2">
+        <div>
+            <b>更新</b>
+        </div>
+        <div v-show="isLoading" class="flex items-center justify-center h-30">
+            <div class="loading"></div>
+        </div>
+        <div class="mt-4 rounded-lg text-base-content/80 border border-base-content/0 p-4 hover:border-base-content/10 hover:bg-base-200"
+            v-for="item in listData" v-motion-slide-visible-right>
+            <div class="flex">
+                <div class="flex-1">
+                    提交者：{{ item.committer.name }}
+                    <span class="text-sm opacity-60 link hover:opacity-90 text-success">
+                        ({{ item.committer.email }})
+                    </span>
+                </div>
+                <div class="text-sm opacity-60">
+                    <span
+                        v-if="new Date().getTime() - new Date(item.committer.date).getTime() > 1000 * 60 * 60 * 24 * 7">
+                        {{ new Date(item.committer.date).toLocaleDateString() }}
+                    </span>
+                    <span v-else>
+                        {{ item.committer.relativeTime.replace('hours ago', '小时前').replace('days ago', '天前')
+                            .replace('minutes ago', '分钟前') }}
+                    </span>
+
+                </div>
+            </div>
+            <div class="inline-flex gap-1 items-center">
+                <span class="link opacity-60 hover:opacity-90 text-accent">#{{ item.shortHash }}</span>
+                <div v-show="item.title.includes(':')" class="badge badge-sm badge-soft" :class="{
+                    'badge-success': item.title.slice(0, item.title.indexOf(':')).includes('feat'),
+                    'badge-info': item.title.slice(0, item.title.indexOf(':')).includes('style'),
+                    'badge-accent': item.title.slice(0, item.title.indexOf(':')).includes('fix'),
+                    'badge-primary': item.title.slice(0, item.title.indexOf(':')).includes('refactor'),
+                }">{{ item.title.slice(0, item.title.indexOf(':')) }}</div>
+                {{ item.title.slice(item.title.indexOf(':') + 1) }}
+                <span v-show="item.body" class="link opacity-60 hover:opacity-90"
+                    @click="showUpdateModal(item)">详情</span>
+            </div>
+        </div>
+        <br>
+        <div class="flex justify-end">
+            <div class="join mr-3" v-show="!isLoading">
+                <button class="btn btn-primary join-item btn-sm btn-soft" @click="btnPreviousPage">上一页</button>
+                <button class="btn btn-primary join-item btn-sm btn-soft" @click="btnNextPage">下一页</button>
+            </div>
+        </div>
+        <br>
+        <modal title="更新详情" :is-show="isShowUpdateModal" @on-backdrop-click="closeUpdateModal">
+            <template #content>
+                <div class="text-base-content/90 mt-2">
+                    <div class="flex">
+                        <div class="flex-1">提交者: {{ updateDetails?.committer.name }}</div>
+                        <div class="text-base-content/60">{{ updateDetails?.shortHash }}</div>
+                    </div>
+                    提交信息: {{ updateDetails?.title }} <br>
+                    详情: {{ updateDetails?.body }}
+                </div>
+            </template>
+            <template #action>
+                <button class="btn btn-sm join-item btn-ghost" @click="closeUpdateModal">关闭</button>
+            </template>
+        </modal>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import * as APP from '~/api/app';
+let listData = ref<APP.Info.GitLogsResultCommitItem[]>([])
+const isShowUpdateModal = ref(false);
+const isLoading = ref(true);
+const updateDetails = ref<APP.Info.GitLogsResultCommitItem>();
+const pageInfo = ref({
+    pageNum: 1,
+    pageSize: 15
+})
+onMounted(() => {
+    initData();
+})
+async function initData() {
+    isLoading.value = true;
+    listData.value = [];
+    const { data } = await APP.Info.gitLogs((pageInfo.value.pageNum - 1) * pageInfo.value.pageSize, pageInfo.value.pageSize);
+    listData.value = data.data.commits;
+    isLoading.value = false;
+}
+function showUpdateModal(item: APP.Info.GitLogsResultCommitItem) {
+    updateDetails.value = item
+    isShowUpdateModal.value = true;
+
+}
+function closeUpdateModal() {
+    isShowUpdateModal.value = false;
+    updateDetails.value = undefined;
+}
+
+function btnNextPage() {
+    if (listData.value.length < pageInfo.value.pageSize) return;
+    pageInfo.value.pageNum++;
+    initData();
+}
+function btnPreviousPage() {
+    if (pageInfo.value.pageNum == 1) return;
+    pageInfo.value.pageNum--;
+    initData();
+}
+
+</script>
--- apps/frontend/pages/index/index.vue
+++ apps/frontend/pages/index/index.vue
@@ -1,20 +1,9 @@
 <template>
-    <div>
-        <button class="btn btn-primary btn-sm" @click="test">test</button>
-        <div v-for="item in listData">
-            {{ item }}
-        </div>
+    <div class="h-full overflow-scroll p-2">
+        <index-commit-list></index-commit-list>
     </div>
 </template>
 
-<script lang="ts" setup>
-import * as APP from '~/api/app';
-let listData = ref<APP.Info.GitLogsResultCommitItem[]>([])
-async function test() {
-    const { data } = await APP.Info.gitLogs(0, 15);
-    listData.value = data.data.commits;
-    console.log(data.data.commits[0].committer.relativeTime);
-
-}
+<script setup lang="ts">
 
 </script>
